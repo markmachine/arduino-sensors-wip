5c5b334e86ef7796049b5cc836a3369ade9cf4d5
mark hughes <mark.hughes@revmecha.com>
2016-07-20 10:27:55 -0500

    sensor node check 2

--- server_stub.js
+++ server_stub.js
@@ -12,7 +12,7 @@ var sp = new serialport.SerialPort(portName, {
 var HOST = 'ws://secure-brushlands-96373.herokuapp.com';
 var WebSocket = require('ws')
   , ws = new WebSocket(HOST);
-var ident = false;
+var ident = false, connected = false;
 
 
 function makeid()
@@ -26,40 +26,54 @@ function makeid()
     return text;
 }
 
+function getID() {
+	if (!ident) {
+		ident = "client_" + makeid();
+		console.log("send id", ident);
+	}
+	return ident;
+}
+
 
 
 sp.on('data', function(input) {
-    console.log(input);
-    var ttype = input.substr(0,1);
-    var value = input.substr(1);
-    if (ttype == "A") {
-        console.log("AUDIO", { type: "device" }, { verb: "sensed" }, { type: "audio", id: value, view: "scale"});
-    } else if (ttype == "L") {
-        console.log("LIGHT", { type: "device" }, { verb: "sensed" }, { type: "light", id: value, view: "scale"});
-    } else if (ttype == "r") {
-        console.log("range", { type: "device" }, { verb: "sensed" }, { type: "range", id: value, view: "cm"});
-    } else if (ttype == "R") {
-        console.log("RANGE", { type: "device" }, { verb: "sensed" }, { type: "range", id: value, view: "cm"});
-    } else if (ttype == "m") {
-        console.log("motion", { type: "device" }, { verb: "sensed" }, { type: "motion", id: value, view: "start"});
-    } else if (ttype == "M") {
-        console.log("MOTION", { type: "device" }, { verb: "sensed" }, { type: "motion", id: value, view: "end"});
-    }
+
+	if (connected) {
+		// console.log(input);
+	    var ttype = input.substr(0,1);
+	    var value = input.substr(1);
+	    var message;
+	    if (ttype == "A") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "audio", id: value, view: "scale"};
+	    } else if (ttype == "L") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "light", id: value, view: "scale"};
+	    } else if (ttype == "r") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "range", id: value, view: "cm"};
+	    } else if (ttype == "R") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "range", id: value, view: "cm"};
+	    } else if (ttype == "m") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "motion", id: value, view: "start"};
+	    } else if (ttype == "M") {
+	        message = { type: "device" }, { verb: "sensed" }, { type: "motion", id: value, view: "end"};
+	    }
+	    if (message != null) {
+	    	ws.send(JSON.stringify({userName: getID(),data: message}));
+	    }
+	}
+	    
 
 });
 
 
-// ws.on('open', function() {
-//     ws.send('something');
-// });
-// ws.on('message', function(message) {
-//     console.log('received: %s', message);
-//     if(!ident) {
-//       ident = "client_" + makeid();
-//       console.log("send id");
-//       ws.send(JSON.stringify({userName:ident,data: "foo_" + makeid() + "_dynamic"}));
+ws.on('open', function() {
+    if(!ident) {
+      ident = "client_" + makeid();
+      ws.send(JSON.stringify({userName: getID(),data: "foo_" + makeid() + "_dynamic"}));
+    }
+});
+ws.on('message', function(message) {
+	console.log("send id", getID());
+	console.log("recieve message", message);
+});
 
-//     } else {
-      
-//     }
-// });
+// EOF
